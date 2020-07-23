41d3ee666b75908f4a7eb742bf2682546b720607
Игорь <igor.volkov1999@gmail.com>
2020-07-24 01:58:25 +0400

    added add/remove product from cart click handlers added product discount label

--- src/components/Main/ProductList/ProductCard.jsx
+++ src/components/Main/ProductList/ProductCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import DiscountLabel from "./DiscountLabel";
 
 const ProductCard = (props) => {
+    const getProductPrice = () => {
+        if (props.discount) {
+            return (
+                <b>
+                    <span className="product-item__price_old">{props.price} рублей</span>
+                    <span> {props.price * (100 - props.discount) / 100} рублей</span>
+                </b>
+            )
+        } else {
+            return <b>{props.price} рублей</b>
+        }
+    };
+
     return (
         <div className="product-item">
+            {props.discount ? <DiscountLabel discount={props.discount}/> : null}
             <div className="product-item__image-container">
                 <img src={props.imageURL} alt="" className="product-item__image"/>
             </div>
@@ -10,8 +25,22 @@ const ProductCard = (props) => {
                 {props.name}
             </p>
             <p className="product-item__code">Артикул: {props.code}</p>
-            <p className="product-item__price">Цена: <b>{props.price} рублей</b></p>
-            <div className="product-item__add-to-cart-btn">Добавить в корзину</div>
+            <p className="product-item__price">
+                Цена: {getProductPrice()}
+            </p>
+            {
+                props.count ?
+                    <div className="product-item__in-cart-count">
+                        <span className="product-item__current-count">В корзине: {props.count}</span>
+                        <span className="product-item__change-count" onClick={props.handleAddToCart}>+</span>
+                        <span className="product-item__change-count" onClick={props.handleRemoveFromCart}>-</span>
+                    </div>
+                    :
+                    <div className="product-item__add-to-cart-btn" onClick={props.handleAddToCart}>
+                        Добавить в корзину
+                    </div>
+            }
+
         </div>
     )
 };
--- src/components/Main/ProductList/ProductList.jsx
+++ src/components/Main/ProductList/ProductList.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import ProductCard from "./ProductCard";
-import PaginationControls from "../News/PaginationControls/PaginationControls";
+import PaginationControls from "../../common/PaginationControls/PaginationControls";
 
 const ProductList = (props) => {
-    console.log(props);
     return (
         <React.Fragment>
             <div className="product-list">
-                {props.productList.map(product => {
-                    return <ProductCard id={product.id}
-                                        imageURL={product.imageURL}
-                                        name={product.name}
-                                        price={product.price}
-                                        code={product.code}
-                                        key={product.id}/>
-                })}
+                {props.productList.length === 0
+                    ? "Увы, товаров нет :("
+                    : props.productList.map(product => {
+                        return <ProductCard id={product.id}
+                                            imageURL={product.imageURL}
+                                            name={product.name}
+                                            price={product.price}
+                                            code={product.code}
+                                            key={product.id}
+                                            count={product.count}
+                                            discount={product.discount}
+                                            handleAddToCart={() => props.addProductToCart(product)}
+                                            handleRemoveFromCart={() => props.removeProductFromCart(product.id)}/>
+                    })}
             </div>
             <PaginationControls pageCount={props.pageCount} currentPage={props.currentPage}
                                 onItemClick={props.onPaginationClick}/>
