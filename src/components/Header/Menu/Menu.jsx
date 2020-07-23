e943175b7c34e0ca684285e00ade1b2e1a9ca0f9
Игорь <igor.volkov1999@gmail.com>
2020-07-24 01:54:15 +0400

    added menu cart item container component

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 
 import MenuItem from "./MenuItem";
+import MenuCartItemContainer from "./MenuCartItem/MenuCartItemContainer";
 
 const Menu = (props) => {
     return (
         <ul className="header__menu">
             {props.menuItemList.map((menuItem, index) => <MenuItem key = {index} title={menuItem.title} href={menuItem.href}/>)}
+            <MenuCartItemContainer/>
         </ul>
     )
 };
